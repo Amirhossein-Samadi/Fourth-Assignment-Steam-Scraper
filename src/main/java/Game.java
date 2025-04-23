cf68cc25c281eeef34cd90a7f676f9d3ee3925b7
Amirhossein Samadi <bookam2025@gmail.com>
2025-04-23 21:28:37 +0330

    implementing the constructor of Game.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,13 +1,16 @@
 import java.util.Objects;
 
 public class Game {
+
     private String name;
     private double rating;
     private int price;
 
-    public Game(String name, double rating, int price) {
-        //TODO
-
+    public Game(String name, double rating, int price)
+    {
+        this.name = name;
+        this.rating = rating;
+        this.price = price;
     }
 
     public String getName() {
@@ -30,7 +33,8 @@ public class Game {
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(Object o)
+    {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Game game = (Game) o;
