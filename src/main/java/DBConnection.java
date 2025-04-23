19031f944e6566a06561cbe99d96e877d9912676
Amirhossein Samadi <bookam2025@gmail.com>
2025-04-23 22:50:57 +0330

    Creating DB.

---
+++ src/main/java/DBConnection.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    public static Connection DBConnection() {
+        Connection conn = null;
+        try {
+            String url = "jdbc:sqlite:C:\\my data\\AP\\Fourth-Assignment-Steam-Scraper\\src\\main\\java\\GamesDb.db";
+            conn = DriverManager.getConnection(url);
+             System.out.println("اتصال به دیتابیس برقرار شد.");
+        } catch (SQLException e) {
+            System.out.println("خطا در اتصال به دیتابیس: " + e.getMessage());
+        }
+        return conn;
+    }
+
+    public static void main(String[] args) {
+        DBConnection();
+    }
+
+}
