7a22d0c1ed938b6263b31d0897b273a2e64b7ab5
Amirhossein Samadi <bookam2025@gmail.com>
2025-04-23 21:37:20 +0330

    Copleting sort by name method.

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -9,14 +9,28 @@ import java.util.*;
 public class Parser {
     static List<Game> games = new ArrayList<>();
 
-    public List<Game> sortByName(){
+    public List<Game> sortByName()
+    {
         List<Game> sortedByName = new ArrayList<>(games);
-        // Sort games alphabetically (least)
-        //TODO
+
+        // Bubble Sort
+        public static void sortByName(List<Game> games) {
+            for (int i = 0; i < games.size() - 1; i++) {
+                for (int j = 0; j < games.size() - i - 1; j++) {
+                    if (games.get(j).getName().compareTo(games.get(j + 1).getName()) > 0) {
+                        Game temp = games.get(j);
+                        games.set(j, games.get(j + 1));
+                        games.set(j + 1, temp);
+                    }
+                }
+            }
+        }
+
         return  sortedByName;
     }
 
-    public List<Game> sortByRating(){
+    public List<Game> sortByRating()
+    {
         List<Game> sortedByRating = new ArrayList<>(games);
         // Sort games by rating (most)
         //TODO
